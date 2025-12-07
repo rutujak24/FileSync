5762c510ef866f923d43eab07093b2d6b6f4c1b8
rkawade3 <rkawade3@gatech.edu>
2025-12-06 22:45:47 -0500

    refactor: Simplify codebase and cleanup storage

--- src/server/server.h
+++ src/server/server.h
@@ -14,9 +14,6 @@ public:
     
     grpc::Status UploadFile(grpc::ServerContext* context, grpc::ServerReader<FileChunk>* reader, UploadResponse* response) override;
     grpc::Status DownloadFile(grpc::ServerContext* context, const FileRequest* request, grpc::ServerWriter<FileChunk>* writer) override;
-    grpc::Status PropagateMeta(grpc::ServerContext* context, const MetadataUpdate* request, PropagateResponse* response) override;
-    grpc::Status FetchShard(grpc::ServerContext* context, const ShardRequest* request, ShardData* response) override;
-    grpc::Status Heartbeat(grpc::ServerContext* context, const HeartbeatRequest* request, HeartbeatResponse* response) override;
     grpc::Status ListFiles(grpc::ServerContext* context, const ListFilesRequest* request, FileListResponse* response) override;
 
 private:
